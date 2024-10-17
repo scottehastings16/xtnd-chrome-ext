e08d0b400f784abec861305ef19af63e108acc9a
scottehastings16 <126291607+scottehastings16@users.noreply.github.com>
2024-10-17 10:33:24 -0500

    Update background.js

--- background.js
+++ background.js
@@ -2,11 +2,14 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.status === 'complete' && /^https?:\/\//i.test(tab.url)) {
     chrome.scripting.executeScript({
       target: { tabId: tabId },
-      files: ['inject.js']
+      function: () => {
+        // Create a script element
+        const script = document.createElement('script');
+        script.src = chrome.runtime.getURL('inject.js'); // Get the URL of inject.js
+
+        // Append the script to the head of the document
+        document.head.appendChild(script);
+      }
     });
   }
 });
-
-
-
-
