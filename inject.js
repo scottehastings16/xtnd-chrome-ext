5267f64d6ceee0ec6e6ff34a61500de432785bcf
scottehastings16 <126291607+scottehastings16@users.noreply.github.com>
2024-10-17 10:40:28 -0500

    Update inject.js

--- inject.js
+++ inject.js
@@ -1,14 +1,21 @@
-(function() {
-  const originalPush = window.dataLayer.push;
-  window.dataLayer.push = function(...args) {
-    originalPush.apply(window.dataLayer, args);
-
-    // Find the extension's tab ID
-    chrome.runtime.sendMessage({ type: 'getTabId' }, (response) => {
-      if (response && response.tabId) {
-        // Send the message to the extension page
-        chrome.tabs.sendMessage(response.tabId, { type: 'dataLayerEvent', data: args });
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === 'complete' && /^https?:\/\//i.test(tab.url)) {
+    chrome.scripting.executeScript({
+      target: { tabId: tabId },
+      function: () => {
+        const script = document.createElement('script');
+        script.src = chrome.runtime.getURL('inject.js'); 
+        document.head.appendChild(script);
       }
     });
-  };
-})();
+  }
+});
+
+// Listen for messages from inject.js
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.type === 'getTabId') {
+    sendResponse({ tabId: sender.tab.id }); 
+  }
+});
+
+
